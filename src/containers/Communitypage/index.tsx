9ac9636b8e3ce52061eb990594b4a3ba2b6e7887
hailiting <hailiting@yeah.net>
2021-12-23 16:48:15 +0800

    add img

--- src/containers/Communitypage/index.tsx
+++ src/containers/Communitypage/index.tsx
@@ -4,36 +4,122 @@ import TitleH2 from "components/TitleH2";
 import TitleContext from "components/TitleContext";
 import styled from "styled-components";
 import { Flex } from "uikit";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper";
+// eslint-disable-next-line
+import "swiper/css/bundle";
+
+SwiperCore.use([Autoplay, Pagination, Navigation]);
+
 const CommunitypageWrap = styled(Flex)`
-  flex: 2;
-  img {
-    display: block;
-    width: 400px;
-    padding-left: 80px;
-  }
+  flex: 1;
+  flex-flow: wrap;
 `;
 
 const LiWrap = styled.div`
-  padding-top: 40px;
-  width: 50%;
+  padding-top: 20px;
+  padding-bottom: 40px;
+  width: 45%;
   ${({ theme }) => theme.mediaQueries.md} {
-    padding-top: 60px;
+    width: 25%;
+    padding-top: 40px;
+    padding-bottom: 40px;
   }
 `;
-const CommunityArr01 = [
+const CommunitypageStyled = styled.div`
+  .swiper {
+    width: 100%;
+    height: 100%;
+  }
+
+  .swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    background: #fff;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  // .swiper-slide img {
+  //   display: block;
+  //   width: 100%;
+  //   object-fit: cover;
+  // }
+
+  .swiper {
+    width: 100%;
+    height: 300px;
+    margin: 0 auto;
+    ${({ theme }) => theme.mediaQueries.xs} {
+      // height: 500px;
+    }
+    ${({ theme }) => theme.mediaQueries.md} {
+      height: 700px;
+    }
+  }
+  .append-buttons {
+    text-align: center;
+    margin-top: 20px;
+  }
+
+  .append-buttons button {
+    display: inline-block;
+    cursor: pointer;
+    border: 1px solid ${({ theme }) => theme.colors.text};
+    color: ${({ theme }) => theme.colors.text};
+    text-decoration: none;
+    padding: 4px 10px;
+    border-radius: 4px;
+    margin: 0 10px;
+    font-size: 13px;
+  }
+  .swiper-button-next:after,
+  .swiper-button-prev:after {
+    color: ${({ theme }) => theme.colors.text};
+    font-size: 20px;
+  }
+  .swiper-horizontal > .swiper-pagination-bullets .swiper-pagination-bullet,
+  .swiper-pagination-horizontal.swiper-pagination-bullets .swiper-pagination-bullet {
+    background-color: ${({ theme }) => theme.colors.text};
+  }
+`;
+const SwiperContainer = styled.div`
+  display: flex;
+  flex-flow: wrap;
+  align-items: stretch;
+  justify-content: space-between;
+  list-style: none;
+  img {
+    margin-bottom: 20px;
+    display: block;
+    &:nth-child(1) {
+      width: 68%;
+    }
+    &:nth-child(2) {
+      width: 30%;
+    }
+    &:nth-child(3) {
+      width: 30%;
+    }
+    &:nth-child(4) {
+      width: 68%;
+    }
+  }
+`;
+const CommunityArr = [
   { number: "150,000+", text: "Community Member" },
   { number: "5000+", text: "Technical Member" },
-];
-const CommunityArr02 = [
   { number: "50+", text: "Global Coop Projects" },
   { number: "200+", text: "Community AMA" },
 ];
 const Communitypage = () => {
   return (
-    <>
+    <CommunitypageStyled>
       <TitleH2>COMMUNITY</TitleH2>
       <CommunitypageWrap>
-        {CommunityArr01.map((v: { number: string; text: string }, index: number) => {
+        {CommunityArr.map((v: { number: string; text: string }, index: number) => {
           return (
             <LiWrap key={index}>
               <TitleH1>{v.number}</TitleH1>
@@ -42,18 +128,35 @@ const Communitypage = () => {
           );
         })}
       </CommunitypageWrap>
-      <CommunitypageWrap>
-        {CommunityArr02.map((v: { number: string; text: string }, index: number) => {
-          return (
-            <LiWrap key={index}>
-              <TitleH1>{v.number}</TitleH1>
-              <TitleContext>{v.text}</TitleContext>
-            </LiWrap>
-          );
-        })}
-      </CommunitypageWrap>
-    </>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        className="mySwiper"
+      >
+        {[0, 1, 2].map((round) => (
+          <SwiperSlide key={round}>
+            <RoundCard value={[round * 4 + 1, round * 4 + 2, round * 4 + 3, round * 4 + 4]} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </CommunitypageStyled>
   );
 };
-
+const RoundCard = ({ value }: { value: number[] }) => {
+  return (
+    <SwiperContainer>
+      {value.map((v) => (
+        <img key={v} src={`/images/community/${v}.webp`} alt={`${v}`} />
+      ))}
+    </SwiperContainer>
+  );
+};
 export default Communitypage;
